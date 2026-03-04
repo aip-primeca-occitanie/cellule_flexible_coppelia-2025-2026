154fa6b9f73b8f67a058799a09cf3d61cf26fc0e
Elvenn <norroy.elvenn@gmail.com>
2026-03-04 08:02:47 +0000

    maj shuttle ttm

---
+++ celluleflexible/sim/lua_files/shuttle/shuttleD.lua
@@ -0,0 +1,95 @@
+function sysCall_init()
+    -- Initialisation des handles
+    self = sim.getObjectHandle(sim.handle_self)
+    myName = sim.getObjectName(self)
+    lettre = myName:sub(8,8)
+    
+    MotorFrontLeftHandle = sim.getObjectHandle('FrontLeft'..lettre)
+    MotorFrontRightHandle = sim.getObjectHandle('FrontRight'..lettre)
+    InductiveSensor = sim.getObjectHandle('InductiveSensor'..lettre)
+    UltrasonicSensor = sim.getObjectHandle('UltrasonicSensor'..lettre)
+    FrontSensor = sim.getObjectHandle('Front_sensor'..lettre)
+
+    -- Handles des produits
+    produitHandles = {}
+    for i=1,6 do
+        produitHandles[i] = sim.callScriptFunction('getHandleInside', sim.scripttype_mainscript, 'Produit'..i..'_'..lettre)
+    end
+
+    -- Couleurs (Table simplifiée pour la lisibilité)
+    colors = {
+        base = {200/255, 200/255, 200/255},
+        [10] = {255/255, 153/255, 153/255}, [11] = {255/255, 102/255, 102/255}, [12] = {255/255, 0/255, 0/255},
+        [20] = {102/255, 178/255, 255/255}, [22] = {0/255, 102/255, 255/255},
+        -- (Gardez le reste de vos couleurs ici)
+    }
+
+    -- Vérification du plugin ROS 2
+    if simROS2 then
+        
+        -- On définit le nom du topic proprement, par exemple : /ShuttleZ_1/set_colors
+        local topicName = '/' .. myName:gsub("#","_") .. '/set_colors'
+        
+        -- Subscriber pour changer les couleurs via ROS2 (SANS createNode)
+        colorSub = simROS2.createSubscription(topicName, 'std_msgs/msg/Int32MultiArray', 'color_callback')
+        
+        -- État initial des couleurs
+        currentColors = {0, 0, 0, 0, 0, 0}
+    else
+        sim.addLog(sim.verbosity_errors, "ROS2 plugin not found!")
+    end
+end
+-- Callback appelée quand ROS 2 envoie un message
+function color_callback(msg)
+    -- msg.data est un tableau de 6 entiers
+    currentColors = msg.data
+end
+
+function sysCall_sensing()
+    -- Mise à jour des couleurs des produits
+    for i=1,6 do
+        updateObjectColor(produitHandles[i], currentColors[i] or 0)
+    end
+end
+
+function updateObjectColor(object, code)
+    local color = colors[code] or colors.base
+    local trans = 1.0
+
+    -- On rétablit la logique de transparence de l'ancien code
+    if code == 0 then
+        trans = 0.0 -- 0 = Totalement transparent (invisible) par défaut
+    elseif code % 10 == 2 then
+        trans = 0.5 -- 0.5 = Semi-transparent pour les codes finissant par 2 (ex: 12, 22...)
+    else
+        trans = 1.0 -- 1.0 = Opaque pour les autres couleurs (10, 11, 20, etc.)
+    end
+
+    sim.setShapeColor(object, nil, sim.colorcomponent_ambient_diffuse, color)
+    sim.setShapeColor(object, nil, sim.colorcomponent_transparency, {trans})
+end
+function sysCall_actuation()
+    local slow = sim.checkProximitySensor(InductiveSensor, sim.handle_all)
+    local stop = sim.checkProximitySensor(UltrasonicSensor, sim.handle_all)
+    local front = sim.checkProximitySensor(FrontSensor, sim.handle_all)
+
+    local leftVel, rightVel = 0, 0
+
+    if (front == 1 or stop == 1) then
+        leftVel, rightVel = 0, 0
+    elseif (slow == 1) then
+        leftVel, rightVel = -10, 10
+    else
+        leftVel, rightVel = -20, 20
+    end
+
+    sim.setJointTargetVelocity(MotorFrontLeftHandle, leftVel)
+    sim.setJointTargetVelocity(MotorFrontRightHandle, rightVel)
+end
+
+function sysCall_cleanup()
+    -- On ferme proprement la souscription au lieu de détruire un Node
+    if simROS2 and colorSub then
+        simROS2.shutdownSubscription(colorSub)
+    end
+end
